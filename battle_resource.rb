18f91bfb6c66b2b9daefac45455e6befd1282407
user <user@applift-mws-198.fritz.box>
2017-07-16 16:17:02 +0200

    applied rules 1 and 2 for war

--- battle_resource.rb
+++ battle_resource.rb
@@ -1,5 +1,9 @@
 module Main
   class BattleResource
+  	
+      attr_accessor :power, :strength
+    
+
     def initialize(power, strength)
       @power = power
       @strength = strength
--- enemy/falicornia.rb
+++ enemy/falicornia.rb
@@ -1,9 +1,14 @@
 require './battle_resource'
 require './battle_power'
+require './lengaburu'
 
 module Enemy
   class Falicornia
 
+      attr_accessor :horse, :elephant, :armoured_tank, :sling_gun
+    
+
+    POWER_MULTIPLIER = 2
     def self.build_enemy
       Enemy::Falicornia.new
     end
@@ -28,7 +33,24 @@ module Enemy
     end
 
     def battle
-      p "the battle with falicornia"
+      h,self.horse.strength = rule_one_two(Main::Lengaburu.horse.strength, self.horse.strength)
+      h,self.elephant.strength = rule_one_two(Main::Lengaburu.elephant.strength, self.elephant.strength)
+      h,self.armoured_tank.strength = rule_one_two(Main::Lengaburu.armoured_tank.strength, self.armoured_tank.strength)
+      h,self.sling_gun.strength = rule_one_two(Main::Lengaburu.sling_gun.strength, self.sling_gun.strength)
+      p self.sling_gun.strength
+    end
+
+    def rule_one_two(l_resource,f_resource)
+      #h: remaining horses for Lengaburu
+       rem = f_resource - (2 * l_resource)
+      if rem > 0
+        f_resource = rem
+        rem = 0;
+      else
+        f_resource = 0
+        rem = (rem.abs/2).floor
+      end
+      return rem,f_resource
     end
   end
 end
